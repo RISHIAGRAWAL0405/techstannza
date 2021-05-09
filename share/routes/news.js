5a80477db736d7575ed7b383d7d826b6e8600dff
Atishay Jain <atishay.bhl@gmail.com>
2021-05-09 18:53:26 +0530

    ready

---
+++ share/routes/news.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const router=express.Router();
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const News=require("../models/news");
+
+router.get("/",async (req,res)=>{
+    let allnews=await News.find({});
+    res.render("news",{allnews});
+});
+
+
+
+module.exports=router;
