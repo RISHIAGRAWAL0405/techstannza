d75beca8bb56e79e0d2e6a39531cc9d55de8ff51
Atishay Jain <atishay.bhl@gmail.com>
2021-06-12 19:27:40 +0530

    ready

--- public/javascript/comparison.js
+++ public/javascript/comparison.js
@@ -1,227 +1,245 @@
-let searchDivision=document.querySelectorAll(".searchResult");
-let fake=document.querySelector(".fake");
-let inputs=document.querySelectorAll(".mobileInput");
-let mobiles=[];
+let upperStrip = document.querySelector(".upper-strip");
+let comparison = document.querySelector(".comparison");
 
-let linkAxios1="http://localhost:3000/axiosMobiles";
-let linkAxios2="https://desolate-badlands-28322.herokuapp.com/axiosMobiles";
-let getmobiles=async()=>{
-    let res=await axios.get(linkAxios2);
-    let data=res.data;
-    console.log(data);
-    for(mobile of data){
-        mobiles.push(mobile);
-    }
-}
-getmobiles();
-      
-inputs.forEach(input=>{
-     input.addEventListener(("input"),()=>{
-        fillUp(input);
-     })
- })
-
-
-function fillUp(input){
-    let divi=input.nextElementSibling;
-    divi.style.display="flex";
-    divi.style.flexDirection="column";
-    let keys=diffkeys((input.value).toLowerCase());
-    // console.log(keys)
-    
-    let response=getrelated(keys);
-    response=removeInitial(response);
-    deleteChild(divi);
-    // console.log(response);
-    for(mobile of response){
-      let newMobile=fake.cloneNode(true);
-      newMobile.childNodes[1].src=mobile.image;
-      newMobile.childNodes[3].childNodes[1].innerText=mobile.name;
-
-      newMobile.classList.add("dynamicClass");
-      divi.append(newMobile);
-    }
-    let mobimage=input.parentNode.parentNode.childNodes[3].childNodes[1];
-    let className=input.parentElement.parentElement.classList[0];
-    let addButtons=input.nextElementSibling.childNodes;
-    // document.querySelectorAll(`.${className} .addButton`);
-    // console.log(addButtons);
-    addButtons.forEach(button => {
-        button.addEventListener("click",()=>{
-            let mob=getMobile(button);
-            mobimage.src=mob.image;
-            fillDown(className,mob);
-            // console.log(mobimage);
-            addNamesInUpperStrip(className,mob);
-        })
-    });
-}
-
-function getrelated(keys){
-    let response=[];
-    let keywords=diffnames(mobiles);
-    for(mobile of mobiles){
-        for(key of keys){
-            // console.log(mobile.name);
-            if(mobile.name.toLowerCase()==key || mobile.name.toLowerCase().split(" ").includes(key)){
-              if(!response.includes(mobile))
-                response.push(mobile);
-            }
-        }
-        
-    }
-    
-   return response;
-};
-
-
-function removeInitial(response){
-   let fMobile=document.querySelector(".firstMobUpper").innerText;
-   let sMobile=document.querySelector(".secondMobUpper").innerText;
-   let tMobile=document.querySelector(".thirdMobUpper").innerText;
-  for(mobile of response){
-      if(mobile.name==fMobile || mobile.name==sMobile || mobile.name==tMobile){
-          response=arrayRemove(response,mobile);
-      }
+document.addEventListener("scroll", () => {
+  if (comparison.getBoundingClientRect().top <= 0) {
+    upperStrip.style.position = "fixed";
+    upperStrip.style.top = 0;
+  } else {
+    upperStrip.style.position = "static";
   }
-  
-  return response;
+});
 
-}
-function arrayRemove(arr, value) { 
-    
-    return arr.filter(function(ele){ 
-        return ele != value; 
+let searchDivision = document.querySelectorAll(".searchResult");
+let fake = document.querySelector(".fake");
+let inputs = document.querySelectorAll(".mobileInput");
+let mobiles = [];
+
+let linkAxios1 = "http://localhost:3000/axiosMobiles";
+let linkAxios2 = "https://desolate-badlands-28322.herokuapp.com/axiosMobiles";
+let getmobiles = async () => {
+  let res = await axios.get(linkAxios1);
+  let data = res.data;
+
+  for (mobile of data) {
+    mobiles.push(mobile);
+  }
+};
+getmobiles();
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    fillUp(input);
+  });
+});
+
+function fillUp(input) {
+  let divi = input.nextElementSibling;
+  divi.style.display = "flex";
+  divi.style.flexDirection = "column";
+  let keys = diffkeys(input.value.toLowerCase());
+  // console.log(keys)
+
+  let response = getrelated(keys);
+  response = removeInitial(response);
+  deleteChild(divi);
+  // console.log(response);
+  for (mobile of response) {
+    let newMobile = fake.cloneNode(true);
+    newMobile.childNodes[1].src = mobile.image;
+    newMobile.childNodes[3].childNodes[1].innerText = mobile.name;
+
+    newMobile.classList.add("dynamicClass");
+    newMobile.classList.remove("fake");
+    divi.append(newMobile);
+  }
+  let mobimage = input.parentNode.parentNode.childNodes[3].childNodes[1];
+  let className = input.parentElement.parentElement.classList[0];
+  let addButtons = input.nextElementSibling.childNodes;
+  // document.querySelectorAll(`.${className} .addButton`);
+  // console.log(addButtons);
+  addButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      let mob = getMobile(button);
+      mobimage.src = mob.image;
+      fillDown(className, mob);
+      // console.log(mobimage);
+      addNamesInUpperStrip(className, mob);
     });
+  });
 }
 
-
-function getMobile(btn){
-    let mobname=btn.childNodes[3].childNodes[1].innerText;
-    // let thismob;
-    for(mobile of mobiles){
-        if(mobile.name===mobname){
-            // console.log(mobile)
-            return mobile;
-          
-        }
+function getrelated(keys) {
+  let response = [];
+  let keywords = diffnames(mobiles);
+  for (mobile of mobiles) {
+    for (key of keys) {
+      // console.log(mobile.name);
+      if (
+        mobile.name.toLowerCase() == key ||
+        mobile.name.toLowerCase().split(" ").includes(key)
+      ) {
+        if (!response.includes(mobile)) response.push(mobile);
+      }
     }
-    
+  }
 
+  return response;
+}
+
+function removeInitial(response) {
+  let fMobile = document.querySelector(".firstMobUpper").innerText;
+  let sMobile = document.querySelector(".secondMobUpper").innerText;
+  let tMobile = document.querySelector(".thirdMobUpper").innerText;
+  for (mobile of response) {
+    if (
+      mobile.name == fMobile ||
+      mobile.name == sMobile ||
+      mobile.name == tMobile
+    ) {
+      response = arrayRemove(response, mobile);
+    }
+  }
+
+  return response;
+}
+function arrayRemove(arr, value) {
+  return arr.filter(function (ele) {
+    return ele != value;
+  });
+}
+
+function getMobile(btn) {
+  let mobname = btn.childNodes[3].childNodes[1].innerText;
+  // let thismob;
+  for (mobile of mobiles) {
+    if (mobile.name === mobname) {
+      // console.log(mobile)
+      return mobile;
+    }
+  }
 }
 
 function deleteChild(e) {
-    
-    
-    
-    var child = e.lastElementChild; 
-    while (child) {
-        e.removeChild(child);
-        child = e.lastElementChild;
+  var child = e.lastElementChild;
+  while (child) {
+    e.removeChild(child);
+    child = e.lastElementChild;
+  }
+}
+
+function diffkeys(query) {
+  return query.split(" ");
+}
+
+function diffnames(mobiles) {
+  let diffnames = [];
+  for (mobile of mobiles) {
+    let respo = diffkeys(mobile.name);
+    for (resp of respo) {
+      diffnames.push(resp);
     }
-}
-
-
-function diffkeys(query){
-    return query.split(" ");
-}
-
-function diffnames(mobiles){
-   let diffnames=[];
-  for(mobile of mobiles){
-      let respo=diffkeys(mobile.name);
-      for(resp of respo){
-          diffnames.push(resp);
-      }
-      
   }
   return diffnames;
 }
 
-let mainSection=document.querySelector(".mainSection");
+let mainSection = document.querySelector(".mainSection");
 
-mainSection.addEventListener("click",()=>{
-   searchDivision[0].style.display="none";
-   searchDivision[1].style.display="none";
-   searchDivision[2].style.display="none";
-  
-})
-let all=document.querySelectorAll(".searchResult div")  
+mainSection.addEventListener("click", () => {
+  searchDivision[0].style.display = "none";
+  searchDivision[1].style.display = "none";
+  searchDivision[2].style.display = "none";
+});
+let all = document.querySelectorAll(".searchResult div");
 
-all.forEach(element => {
-    element.addEventListener("click",(e)=>{
-        
-       e.stopPropagation();
-    })
+all.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
 });
 
-
-
-
-function fillDown(className,mob){
-   let whole=document.querySelector(`.${className} .indiDetails`);
-   whole.style.opacity=1;
-    let frontcamera=document.querySelectorAll(`.${className} .frontcamera td`);
-   let display=document.querySelectorAll(`.${className} .display td`);
-   let performance=document.querySelectorAll(`.${className} .performance td`);
-   frontcamera[0].innerText=mob.frontCamera.pixels[0].pixel+"MP";
-   frontcamera[2].innerText=mob.frontCamera.pixels[0].description;
-   
-
+function fillDown(className, mob) {
+  let whole = document.querySelector(`.${className} .indiDetails`);
+  whole.style.opacity = 1;
+  let frontcamera = document.querySelectorAll(`.${className} .frontcamera td`);
+  let maincamera = document.querySelectorAll(`.${className} .frontcamera td`);
+  let display = document.querySelectorAll(`.${className} .display td`);
+  let performance = document.querySelectorAll(`.${className} .performance td`);
+  let memory = document.querySelectorAll(`.${className} .memory td`);
+  let battery = document.querySelectorAll(`.${className} .battery td`);
+  let network = document.querySelectorAll(`.${className} .network td`);
+  let dimensions = document.querySelectorAll(`.${className} .dimensions td`);
+  let ui = document.querySelectorAll(`.${className} .userInterface td`);
+  let others = document.querySelectorAll(`.${className} .others td`);
+  frontcamera[0].innerText = mob.frontCamera.pixels[0].pixel + "MP";
+  frontcamera[2].innerText = mob.frontCamera.pixels[0].description;
+  display[0].innerText = mob.Display.size;
+  display[1].innerText = mob.Display.resolution;
+  display[2].innerText = mob.Display.category;
+  console.log(mob.refreshrate);
+  display[3].innerText = `${mob.refreshrate}` + "Hz";
+  performance[0].innerText = mob.processor.category;
+  performance[1].innerText = mob.processor.Core;
+  performance[2].innerText = mob.processor.clockSpeed;
+  battery[0].innerText = mob.BatteryCapacity + "Mah";
+  network[0].innerText = mob.network.category;
+  network[1].innerText = mob.network.bluetoothVersion;
+  network[2].innerText = mob.network.nfc;
+  memory[0].innerText = mob.memory.internalStorage;
+  memory[1].innerText = mob.memory.ram;
+  memory[2].innerText = mob.memory.expandableStorage;
+  ui[0].innerText = mob.userInterface;
+  ui[1].innerText = mob.processor.operatingSystem;
+  ui[2].innerText = mob.Display.GPU;
+  dimensions[0].innerText = mob.Dimensions.width;
+  dimensions[1].innerText = mob.Dimensions.height;
+  dimensions[2].innerText = mob.Dimensions.weight;
+  others[0].innerText = mob.sound;
+  others[1].innerText = mob.simType;
+  others[2].innerText = mob.network.infrared;
+  others[3].innerText = mob.network.audioJack;
+  others[4].innerText = mob.Box;
 }
 
+let buttons = document.querySelectorAll(".customButton");
 
-let buttons=document.querySelectorAll(".customButton");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let className = button.parentElement.parentElement.classList[0];
+    removeDown(className);
+    removeNamesInUpperStrip(className);
+  });
+});
 
-
-buttons.forEach(button=>{
-   button.addEventListener("click",()=>{
-    let className=button.parentElement.parentElement.classList[0];
-      removeDown(className);
-      removeNamesInUpperStrip(className);
-    })
-})
-
-
-function removeDown(className){
-    let whole=document.querySelector(`.${className} .indiDetails`);
-    whole.style.opacity=0.2;
-    let imageSection=document.querySelector(`.${className} .originalImage`);
-    imageSection.src="";
-    let frontcamera=document.querySelectorAll(`.${className} .frontcamera td`);
-   let display=document.querySelectorAll(`.${className} .display td`);
-   let performance=document.querySelectorAll(`.${className} .performance td`);
-   frontcamera[0].innerText="";
-   frontcamera[2].innerText="";
+function removeDown(className) {
+  let whole = document.querySelector(`.${className} .indiDetails`);
+  whole.style.opacity = 0.2;
+  let imageSection = document.querySelector(`.${className} .originalImage`);
+  imageSection.src = "";
+  let frontcamera = document.querySelectorAll(`.${className} .frontcamera td`);
+  let display = document.querySelectorAll(`.${className} .display td`);
+  let performance = document.querySelectorAll(`.${className} .performance td`);
+  frontcamera[0].innerText = "";
+  frontcamera[2].innerText = "";
 }
 
-function addNamesInUpperStrip(classname,mob){
-   let selector;
-//    console.log(classname);
-   if(classname=="firstMobile")
-      selector=".firstMobUpper";
-    if(classname=="secondMobile")
-      selector=".secondMobUpper";
-    if(classname=="thirdMobile")
-      selector=".thirdMobUpper";
-   
+function addNamesInUpperStrip(classname, mob) {
+  let selector;
+  //    console.log(classname);
+  if (classname == "firstMobile") selector = ".firstMobUpper";
+  if (classname == "secondMobile") selector = ".secondMobUpper";
+  if (classname == "thirdMobile") selector = ".thirdMobUpper";
 
-    let spaniore=document.querySelector(selector);
-    spaniore.innerText=mob.name;  
-
+  let spaniore = document.querySelector(selector);
+  spaniore.innerText = mob.name;
 }
-function removeNamesInUpperStrip(classname){
-    let selector;
-    // console.log(classname);s
-    if(classname=="firstMobile")
-       selector=".firstMobUpper";
-     if(classname=="secondMobile")
-       selector=".secondMobUpper";
-     if(classname=="thirdMobile")
-       selector=".thirdMobUpper";
-    
- 
-     let spaniore=document.querySelector(selector);
-     spaniore.innerText="Add Mobile"; 
-}
+function removeNamesInUpperStrip(classname) {
+  let selector;
+  // console.log(classname);s
+  if (classname == "firstMobile") selector = ".firstMobUpper";
+  if (classname == "secondMobile") selector = ".secondMobUpper";
+  if (classname == "thirdMobile") selector = ".thirdMobUpper";
 
+  let spaniore = document.querySelector(selector);
+  spaniore.innerText = "Add Mobile";
+}
