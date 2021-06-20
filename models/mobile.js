86ffe60ec1d8c2035296133589b916056dd338e3
Atishay Jain <atishay.bhl@gmail.com>
2021-06-20 16:52:42 +0530

    ready

--- models/mobile.js
+++ models/mobile.js
@@ -1,10 +1,9 @@
-const mongoose=require("mongoose");
-
+const mongoose = require("mongoose");
 
 // const connectionParams={
 //     useNewUrlParser: true,
 //     useCreateIndex: true,
-//     useUnifiedTopology: true 
+//     useUnifiedTopology: true
 // }
 // mongoose.connect("mongodb+srv://Atishay:WMTU4NnUcnka5rm6@cluster0.upzac.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",connectionParams)
 //     .then( () => {
@@ -14,151 +13,110 @@ const mongoose=require("mongoose");
 //         console.error(`Error connecting to the database. \n${err}`);
 //     })
 
-
-
-const mobileSchema=new mongoose.Schema({
-    
-    
-    name:{
-        type:String,
-        required:true
+const mobileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  mainCamera: {
+    noOfCameras: {
+      type: Number,
+      required: true,
     },
-    price:{
-        type:Number,
-        required:true
-    },
-    mainCamera:{
-        noOfCameras:{
-          type:Number,
-          required:true     
+    pixels: [
+      {
+        pixel: {
+          type: Number,
         },
-        pixels:[
-            {
-                pixel:{
-                    type:Number,
-                    
-                },
-                description:{
-                    type:String,
-                    
-                }
-       
-     
-            }
-        ]
-    },
-    frontCamera:{
-            noOfCameras:{
-              type:Number,
-              required:true     
-            },
-            pixels:[
-                {
-                    pixel:{
-                        type:Number
-                        
-                    },
-                    description:{
-                        type:String
-                    }
-           
-         
-                }
-            ],
-    },
-    brand:{
-        type:String,
-        required:true
-    },
-    image:{
-        type:String,
-        required:true
-    },
-    launchDate:{
-        type:Date,
-        required:true
-    },
-    sound:{
-        type:String
-    },
-    simType:{
-        type:String
-    },
-    Display:{
-        size:String,
-        resolution:String,
-        GPU:String,
-        category:String,
-        other:String
-    },
-    processor:{
-        operatingSystem:String,     //Android 10
-        category:String,               //mediatek dimensity 800U
-        Core:String,               //octa core
-        clockSpeed:String,         //2.4GHz
-
-    },
-    memory:{
-        internalStorage:String,     
-        ram:String,
-        expandableStorage:String,
-        
-    },
-    network:{
-        category:String,
-        bluetoothVersion:String,
-        nfc:String,
-        infrared:String,
-        audioJack:String,
-
-
-    },
-    BatteryCapacity:String,
-    Dimensions:{
-        width:Number,
-        height:Number,
-        weight:Number
-    },
-    warrantyPeriod:Number,
-    userInterface:String,
-    Box:String,
-    colorVariants:[
-        {
-            type:String
-        }
+        description: {
+          type: String,
+        },
+      },
     ],
-    bodyType:String,
-    review:String,
-    ourOpinion:String
-    
-    
-
-
-
-
-
-    
-
-
-
-
-
-
-   
-              
-        
-           
-
-        
-        
-    
-
-    
-})
-const Mobile=mongoose.model("Mobile",mobileSchema); 
-module.exports=Mobile;
-
-
+  },
+  frontCamera: {
+    noOfCameras: {
+      type: Number,
+      required: true,
+    },
+    pixels: [
+      {
+        pixel: {
+          type: Number,
+        },
+        description: {
+          type: String,
+        },
+      },
+    ],
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  sound: {
+    type: String,
+  },
+  simType: {
+    type: String,
+  },
+  Display: {
+    size: String,
+    resolution: String,
+    GPU: String,
+    category: String,
+    other: String,
+  },
+  processor: {
+    operatingSystem: String, //Android 10
+    category: String, //mediatek dimensity 800U
+    Core: String, //octa core
+    clockSpeed: String, //2.4GHz
+  },
+  memory: {
+    internalStorage: String,
+    ram: String,
+    expandableStorage: String,
+  },
+  network: {
+    category: String,
+    bluetoothVersion: String,
+    nfc: String,
+    infrared: String,
+    audioJack: String,
+  },
+  BatteryCapacity: String,
+  Dimensions: {
+    width: Number,
+    height: Number,
+    weight: Number,
+  },
+  warrantyPeriod: Number,
+  userInterface: String,
+  Box: String,
+  colorVariants: [
+    {
+      type: String,
+    },
+  ],
+  bodyType: String,
+  review: String,
+  ourOpinion: String,
+});
+const Mobile = mongoose.model("Mobile", mobileSchema);
+module.exports = Mobile;
 
 // const insertMobile=async ()=>{
 //     const mobile=new Mobile({name:"iphone 12 pro max",image:"https://res.cloudinary.com/donu29xwi/image/upload/v1619075899/mobiles/iiiii12pro_max_eoeqme.png",price:111000,mainCamera:{noOfCameras:3,pixels:[{pixel:13,description:"very good wide angle lens"}]},frontCamera:{noOfCameras:1,pixels:[{pixel:8,description:"nice"}]},brand:"apple"});
@@ -167,15 +125,11 @@ module.exports=Mobile;
 //     const mobile3=new Mobile({name:"samsung galaxy s21 ultra",price:78000,image:"https://res.cloudinary.com/donu29xwi/image/upload/v1619076121/mobiles/samsung_galxy_s21_o5pvlw.jpg",mainCamera:{noOfCameras:4,pixels:[{pixel:64,description:"ok"}]},frontCamera:{noOfCameras:1,pixels:[{pixel:16,description:"nice"}]},brand:"samsung"});
 //     const mobile4=new Mobile({name:"one plus 9 pro",price:40000,image:"https://res.cloudinary.com/donu29xwi/image/upload/v1619075961/mobiles/OnePlus-9-Pro-5G-renders_q2801j.jpg",mainCamera:{noOfCameras:4,pixels:[{pixel:64,description:"ok"}]},frontCamera:{noOfCameras:1,pixels:[{pixel:16,description:"nice"}]},brand:"one plus"});
 
-       
 //     await mobile.save();
 //     await mobile1.save();
 //     await mobile2.save();
 //     await mobile3.save();
 //     await mobile4.save();
 // }
-
-
-
 
 // insertMobile();
--- public/javascript/content.js
+++ public/javascript/content.js
@@ -1,232 +1,102 @@
 // **********image change on hover*********
 
+let images = document.querySelectorAll(".images-inner img");
+let image = document.querySelector(".image-div img");
+images[0].style.border = "2px solid blue";
+images.forEach((i) => {
+  i.addEventListener("mouseover", () => {
+    images.forEach((im) => {
+      im.style.border = "none";
+    });
 
-let images=document.querySelectorAll(".images-inner img");
-let image=document.querySelector(".image-div img");
-images[0].style.border="2px solid blue";
-images.forEach((i)=>{
-    i.addEventListener("mouseover",()=>{
-         images.forEach((im)=>{
-            im.style.border="none";
-        })
+    setTimeout(() => {
+      image.src = i.src;
+    }, 200);
 
-        setTimeout(()=>{
-            image.src=i.src;
-        },200);
-        
-        
-        i.style.border="2px solid blue";
-
-       
-    })
-   
-   
+    i.style.border = "2px solid blue";
+  });
 });
 
 // ********share Button functionality**********
-let shareBox=document.querySelector(".share-button");
-let shareSpan=document.querySelector(".share-button span");
-let str=window.location.href;
+// let shareBox = document.querySelector(".share-button");
+// let shareSpan = document.querySelector(".share-button span");
+// let str = window.location.href;
 
-shareBox.addEventListener("click",()=>{
-    const el = document.createElement('textarea');
-    el.value = str;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    shareSpan.innerText="copied!";
-    shareSpan.style.color="white";
-    shareSpan.parentElement.style.backgroundColor="green";
-});
-
-
+// shareBox.addEventListener("click", () => {
+//   const el = document.createElement("textarea");
+//   el.value = str;
+//   document.body.appendChild(el);
+//   el.select();
+//   document.execCommand("copy");
+//   document.body.removeChild(el);
+//   shareSpan.innerText = "copied!";
+//   shareSpan.style.color = "white";
+//   shareSpan.parentElement.style.backgroundColor = "green";
+// });
 
 // **********animation Part*********
 AOS.init();
 
+gsap.to(".sunflower", {
+  translateX: "0",
+  opacity: 1,
+  duration: 2,
+  ease: Power1.easeInOut,
+  //   stagger: {
+  //     opacity: 1,
+  //     from: "end",
+  //     amount: 1.5,
+  //   },
+});
 
-    gsap.to(".sunflower",{translateX:"0",opacity:1,duration:1,stagger:{
-      translateX:"100px",
-      from:"end",
-      amount:1.5
-    }});
+let animationForMore = gsap.to(".more", {
+  duration: 1,
+  opacity: 1,
+  height: "1000px",
+  stagger: {
+    from: "end",
+    amount: 1,
+  },
+});
 
-
-
-    let animationForMore=gsap.to(".more",{duration:1,opacity:1,height:"1000px",
-        stagger:{
-            from:"end",
-            amount:1
-        },
-        
-       
-    })
-
-    animationForMore.pause();
-
-
-
-   
-   
+animationForMore.pause();
 
 //the more button functionality
-    let more=document.querySelector(".more");
-    let spanButton=document.querySelector(".moreButton");
-    function expand(){
-     
-      if(more.style.display=="flex"){
-          
-           more.style.display="none";
-           spanButton.innerText="more"; 
-      } 
-      else{      
-            
-           animationForMore.restart(); 
-        //    animationForMore.pause();
-           more.style.display="flex";
+let more = document.querySelector(".more");
+let spanButton = document.querySelector(".moreButton");
+function expand() {
+  if (more.style.display == "flex") {
+    more.style.display = "none";
+    spanButton.innerText = "more";
+  } else {
+    animationForMore.restart();
+    //    animationForMore.pause();
+    more.style.display = "flex";
 
-            spanButton.innerText="close"; 
-      }
-          
-    };
-
-
-
-
-//  *********canvas drawing********? 
-
-
-
-
-
-document.onload=draw();
-  
-
-
-
-function draw(){
-    let canvas=document.querySelector("#points");
-    let wWidth=()=>window.innerWidth;
-    let cWidth=()=>canvas.width/4;
-
-   let ctx=canvas.getContext("2d");
-   canvas.width=window.innerWidth*0.6;
-   window.onload=()=>{
-    canvas.width=window.innerWidth*0.6;
-    if(window.innerWidth<1000){
-        canvas.width=0.9*window.innerWidth;
-    }
-    let x=1;
-    let constant=3*(Math.PI/2)-x*(Math.PI/5);
-    ctx.beginPath();
-    ctx.arc(cWidth()/2,80,30,0,Math.PI*2);
-    ctx.moveTo(3/2*cWidth()+30,80);
-    ctx.arc(3/2*cWidth(),80,30,0,Math.PI*2);
-    ctx.moveTo(5/2*cWidth()+30,80);
-    ctx.arc(5/2*cWidth(),80,30,0,Math.PI*2); 
-    ctx.moveTo(7/2*cWidth()+30,80);
-    ctx.arc(7/2*cWidth(),80,30,0,Math.PI*2);
-    ctx.strokeStyle="lightgrey";
-    ctx.lineWidth=5;
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(cWidth()/2,50);
-    ctx.arc(cWidth()/2,80,30,Math.PI*3/2,constant,true);
-    ctx.moveTo(3/2*cWidth(),50);
-    ctx.arc(3/2*cWidth(),80,30,Math.PI*3/2,constant,true);
-    ctx.moveTo(5/2*cWidth(),50);
-    ctx.arc(5/2*cWidth(),80,30,Math.PI*3/2,constant,true);
-    ctx.moveTo(7/2*cWidth(),50);
-    ctx.arc(7/2*cWidth(),80,30,Math.PI*3/2,constant,true);
-    ctx.strokeStyle='green';
-    ctx.lineWidth=5;
-    ctx.stroke();
-
-   }
-   window.onresize=()=>{
-     canvas.width=window.innerWidth*0.6;
-     if(window.innerWidth<1000){
-         canvas.width=0.9*window.innerWidth;
-     }
-     let x=1;
-     let constant=3*(Math.PI/2)-x*(Math.PI/5);
-     ctx.beginPath();
-     ctx.arc(cWidth()/2,80,30,0,Math.PI*2);
-     ctx.moveTo(3/2*cWidth()+30,80);
-     ctx.arc(3/2*cWidth(),80,30,0,Math.PI*2);
-     ctx.moveTo(5/2*cWidth()+30,80);
-     ctx.arc(5/2*cWidth(),80,30,0,Math.PI*2); 
-     ctx.moveTo(7/2*cWidth()+30,80);
-     ctx.arc(7/2*cWidth(),80,30,0,Math.PI*2);
-     ctx.strokeStyle="lightgrey";
-     ctx.lineWidth=5;
-     ctx.stroke();
-     ctx.beginPath();
-     ctx.moveTo(cWidth()/2,50);
-     ctx.arc(cWidth()/2,80,30,Math.PI*3/2,constant,true);
-     ctx.moveTo(3/2*cWidth(),50);
-     ctx.arc(3/2*cWidth(),80,30,Math.PI*3/2,constant,true);
-     ctx.moveTo(5/2*cWidth(),50);
-     ctx.arc(5/2*cWidth(),80,30,Math.PI*3/2,constant,true);
-     ctx.moveTo(7/2*cWidth(),50);
-     ctx.arc(7/2*cWidth(),80,30,Math.PI*3/2,constant,true);
-     ctx.strokeStyle='green';
-     ctx.lineWidth=5;
-     ctx.stroke();
-     
-     
-   }
-
-   
-   
-  
-   let x=1;
-   let constant=3*(Math.PI/2)-x*(Math.PI/5);
-   ctx.beginPath();
-   ctx.arc(cWidth()/2,80,30,0,Math.PI*2);
-   ctx.moveTo(3/2*cWidth()+30,80);
-   ctx.arc(3/2*cWidth(),80,30,0,Math.PI*2);
-   ctx.moveTo(5/2*cWidth()+30,80);
-   ctx.arc(5/2*cWidth(),80,30,0,Math.PI*2); 
-   ctx.moveTo(7/2*cWidth()+30,80);
-   ctx.arc(7/2*cWidth(),80,30,0,Math.PI*2);
-   ctx.strokeStyle="lightgrey";
-   ctx.lineWidth=5;
-   ctx.stroke();
-   ctx.beginPath();
-   ctx.moveTo(cWidth()/2,50);
-   ctx.arc(cWidth()/2,80,30,Math.PI*3/2,constant,true);
-   ctx.moveTo(3/2*cWidth(),50);
-   ctx.arc(3/2*cWidth(),80,30,Math.PI*3/2,constant,true);
-   ctx.moveTo(5/2*cWidth(),50);
-   ctx.arc(5/2*cWidth(),80,30,Math.PI*3/2,constant,true);
-   ctx.moveTo(7/2*cWidth(),50);
-   ctx.arc(7/2*cWidth(),80,30,Math.PI*3/2,constant,true);
-   ctx.strokeStyle='green';
-   ctx.lineWidth=5;
-   ctx.stroke();
-   
-
-   
-   
-   
-
-    
-
-
-   
-   
-
-
+    spanButton.innerText = "close";
+  }
 }
 
+//  *********canvas drawing********?
 
-let canvas=document.querySelector("#points");
+//
 
+let ratings = document.querySelectorAll(".rating");
 
+ratings.forEach(async (e) => {
+  var bar = new ProgressBar.Circle(e, {
+    color: "#4D61FC",
+    trailColor: "#dee2e6",
+    trailWidth: 10,
+    duration: 1400,
+    easing: "easeInOut",
+    strokeWidth: 10,
+    from: { color: "#8DA5FD", a: 0 },
+    to: { color: "#4D61FC", a: 1 },
 
-
-
-
-
+    step: function (state, circle) {
+      circle.path.setAttribute("stroke", state.color);
+    },
+  });
+  bar.animate(0.7, { duration: 1500 });
+});
