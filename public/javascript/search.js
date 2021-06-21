f80f7e7811a269c44e1c2c0d200deffcbdba3ff8
Atishay Jain <atishay.bhl@gmail.com>
2021-06-21 16:47:07 +0530

    ready

---
+++ public/javascript/search.js
@@ -0,0 +1,46 @@
+let ratings = document.querySelectorAll(".rating");
+
+ratings.forEach(async (e) => {
+  let i = 0;
+  window.addEventListener("scroll", () => {
+    if (e.getBoundingClientRect().top <= 750) {
+      if (i == 0) {
+        var bar = new ProgressBar.Circle(e, {
+          color: "#000",
+          trailColor: "#dfeeea",
+          trailWidth: 10,
+          duration: 1400,
+          easing: "easeInOut",
+          strokeWidth: 10,
+          from: { color: "#FFEA82", a: 0 },
+          to: { color: "#289672", a: 1 },
+
+          step: function (state, circle) {
+            circle.path.setAttribute("stroke", state.color);
+          },
+        });
+        bar.animate(0.7, { duration: 1500 });
+        i++;
+      }
+    }
+  });
+});
+
+let cards = document.querySelectorAll(".card-c");
+
+cards.forEach((e) => {
+  let link = e.childNodes[1].innerText;
+  e.addEventListener("click", () => {
+    window.location.href = `/${link}`;
+  });
+});
+
+window.onload = () => {
+  window.scrollTo(0, 2);
+};
+
+let lastBuy = document.querySelector(".each:nth-of-type(3) .buy-hover");
+
+lastBuy.addEventListener("mouseover", () => {
+  lastBuy.childNodes[3].style.display = "flex";
+});
